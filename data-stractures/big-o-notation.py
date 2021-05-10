2dc7dd44b2c9e13de5f232d461f37dc4ec89fd68
BikoPython <stevennyandoya@gmail.com>
2021-05-10 18:17:32 +0300

    data-stractures:big-O-notation

---
+++ data-stractures/big-o-notation.py
@@ -0,0 +1,7 @@
+# Big O notation is the way to measure how software program's running time or space requirements grow as 
+# the input size grows. We can't measure this using absolute terms such as time in seconds because different 
+# computers have different hardware hence we need a mathematical way to measure time complexity of a program 
+# and Big O is that mathematical way. The basic idea is to come up with mathematical function for a running 
+# time and consider only fastest growing term and discard other terms as well as constants. Big O is used to 
+# measure space complexity as well,
+
